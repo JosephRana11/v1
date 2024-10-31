aba3a7ce441cec991e89dbc9d6254967fcfda72f
Joseph Rana <josephranamgr7@gmail.com>
2024-11-01 01:27:26 +0545

    feat: add about me

---
+++ app/about.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+
+export default function About(){
+    return (
+        <article className="flex flex-col gap-4">
+  <h1 className="custom-subheading">About Me</h1>
+  <p className="mb-4">
+    I began coding in high school, creating small apps in my spare time. I did not think of it anything much back then and took it as a hobby.
+     After graduating, I got into web development and have been building web apps ever since.
+  </p>
+  <p className="mb-4">
+    Outside of work, I’m focused on personal projects and exploring my interests in blockchain and microservices. I am also intersted in solving coding problems.
+  </p>
+  <p className="mb-4">
+    When I'm not in front of a screen, you can usually find me on the basketball court or hiking in the nearby mountains.
+  </p>
+  {/* <div className="flex justify-center">
+      <ProfileImage/>
+  </div> */}
+</article>
+
+    )
+}
+
+const ProfileImage = () => {
+    return (
+        <div className="relative justify-end items-end flex">
+            <Image src={"/assets/images/profile.jpg"} alt="Joseph Rana" width={300} height={300} className="grayscale aspect-square object-cover rounded-xl object-top"/>
+            <Image src={"/assets/images/profile.jpg"} alt="Joseph Rana" width={300} height={300} className="absolute top-10 right-10  aspect-square object-cover rounded-xl object-top"/>
+        </div>
+
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 import { BlogPosts } from 'app/components/posts'
 import Link from 'next/link'
 import Home from './home'
+import About from './about'
 
 export default function Page() {
   return (
+    <>
     <section className='h-[100vh] justify-center flex flex-col'>
       <Home/>
     </section>
+    <section>
+      <About/>
+    </section>
+    </>
   )
 }
 
